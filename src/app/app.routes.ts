9ac067467bfc68303cb7a6ea92c96c66cf7eea1c
WalidDev91 <walid.boulima.dev@outlook.com>
2026-04-14 17:33:12 +0100

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Drivers } from './pages/drivers/drivers';
+import { Vehicles } from './pages/vehicles/vehicles';
+import { Missions } from './pages/missions/missions';
+import { Documents } from './pages/documents/documents';
+import { Administration } from './pages/administration/administration';
+
+export const routes: Routes = [
+  { path: '', component: Dashboard },
+
+  { path: 'dashboard', component: Dashboard },
+  { path: 'drivers', component: Drivers },
+  { path: 'vehicles', component: Vehicles },
+  { path: 'missions', component: Missions },
+  { path: 'documents', component: Documents },
+  { path: 'administration', component: Administration },
+];
