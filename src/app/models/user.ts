c3ec522173f6d2ae7bd8298bebe784f7554a25f9
WalidDev91 <walid.boulima.dev@outlook.com>
2026-06-16 15:00:39 +0100

    Fix users

---
+++ src/app/models/user.ts
@@ -0,0 +1,13 @@
+export interface User {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    role: 'SUPER_ADMIN' | 'ADMIN' | 'FLEET_MANAGER' | 'DRIVER';
+    isValidate: boolean;
+    adminId: string;
+    adminName: string;
+    createdAt: string;
+    updatedAt: string;
+}
--- src/app/pages/users/users.ts
+++ src/app/pages/users/users.ts
@@ -1,28 +1,29 @@
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { UserService } from '../../services/user.service';
+import { User } from '../../models/user';
+
 
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './users.html',
   styleUrl: './users.css',
 })
 export class Users implements OnInit {
 
-  users: any[] = [];
+  users = signal<User[]>([]);
 
-  constructor(private user: UserService) {}
+  constructor(private userService: UserService) { }
 
-  ngOnInit() {
-    this.user.getAll().subscribe({
+  ngOnInit(): void {
+    this.userService.getAll().subscribe({
       next: (data) => {
-        console.log('Users from backend:', data);
-        this.users = data;
+        this.users.set(data);
       },
       error: (err) => {
-        console.error('Error:', err);
+        console.error('User error:', err);
       }
     });
   }
